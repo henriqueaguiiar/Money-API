c43a3b87954723cee8808eaca1485ee3c7f46395
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-02-18 23:30:33 -0300

    refactor(core): adjust program reading responsibility by removing duplicate code and implementing inheritance and overloading

--- src/com/henriqueaguiiar/application/Program.java
+++ src/com/henriqueaguiiar/application/Program.java
@@ -1,43 +1,44 @@
 package com.henriqueaguiiar.application;
 
-import com.henriqueaguiiar.entities.CoinAPI;
-import com.henriqueaguiiar.entities.CoinService;
+import com.henriqueaguiiar.entities.CotacaoService;
 import com.henriqueaguiiar.entities.CryptoAPI;
-import com.henriqueaguiiar.entities.CryptoService;
-
+import com.henriqueaguiiar.entities.CoinAPI;
+import com.henriqueaguiiar.interfaces.CotacaoAPI;
 
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        int consulta = 0;
-        String criptomoeda, coin;
+        int consulta;
 
-        System.out.println("**********  CRYPTO API SERVICE  **********");
-        System.out.println("Digite o numero para selecionar a consulta: ");
-        System.out.println("0 - Criptomoeda\n1 - Moeda");
-        consulta = input.nextInt();
-        input.nextLine();
+        System.out.println("**********  MONEY API SERVICE  **********\n");
 
-        if (consulta == 0) {
-            System.out.print("Digite a criptomoeda para exibir a cotação: ");
-            criptomoeda = input.nextLine();
-            CryptoAPI api = new CryptoAPI();
-            CryptoService service = new CryptoService(api);
-            service.exibirCotacao(criptomoeda);
-        }else if(consulta == 1){
-            System.out.println("Digite uma moeda para exibir a cotação:  ");
-            coin = input.nextLine();
-            CoinAPI coinAPI = new CoinAPI();
-            CoinService coinService = new CoinService(coinAPI);
-            coinService.exibirCotacao(coin);
-        }else{
-            System.out.println("Opção inválida");
+        while (true) {
+            System.out.println("Digite o número para selecionar a consulta: ");
+            System.out.println("0 - Criptomoeda\n1 - Moeda");
+            consulta = input.nextInt();
+            input.nextLine();
+
+            if (consulta == 0 || consulta == 1) {
+                break;
+            }
+            System.out.println("Opção inválida. Tente novamente.");
         }
+
+        System.out.print("Digite a moeda para exibir a cotação: ");
+        String moeda = input.nextLine();
+
+        CotacaoAPI api;
+        if (consulta == 0) {
+            api = new CryptoAPI();
+        } else {
+            api = new CoinAPI();
+        }
+
+        CotacaoService service = new CotacaoService(api);
+        service.exibirCotacao(moeda);
 
         input.close();
     }
 }
-
-
--- src/com/henriqueaguiiar/entities/CoinAPI.java
+++ src/com/henriqueaguiiar/entities/CoinAPI.java
@@ -1,18 +1,18 @@
 package com.henriqueaguiiar.entities;
 
+import com.henriqueaguiiar.interfaces.CotacaoAPI;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class CoinAPI {
-    public String getCotacao(String coin) throws Exception {
-        String endereco = "https://economia.awesomeapi.com.br/json/last/" + coin;
+public class CoinAPI implements CotacaoAPI {
+    @Override
+    public String getCotacao(String moeda) throws Exception {
+        String endereco = "https://economia.awesomeapi.com.br/json/last/" + moeda;
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return response.body();
     }
-
-
 }
